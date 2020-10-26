6754d268658deb3903ea0039c03586dc3cbecbeb
M Senthilnathan (IT/Hosur/TVSMotor) <M.Senthilnathan@tvsmotor.com>
2020-10-26 21:37:41 +0530

    database config created and committed

---
+++ config/database.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    
+    url : 'mongodb://127.0.0.1:27017/TRACE',
+    options : this.options
+ }
+ git 
+ let options = {
+     server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+     replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+     useNewUrlParser : true
+   };
+  
