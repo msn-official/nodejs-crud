abdf393986dbbc07da206233771fdae506678c46
M Senthilnathan (IT/Hosur/TVSMotor) <M.Senthilnathan@tvsmotor.com>
2020-10-26 21:43:20 +0530

    database config created and committed

--- config/database.config.js
+++ config/database.config.js
@@ -3,7 +3,7 @@ module.exports = {
     url : 'mongodb://127.0.0.1:27017/TRACE',
     options : this.options
  }
- git 
+ 
  let options = {
      server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
      replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
