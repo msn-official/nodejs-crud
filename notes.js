d7a1af820f4316d3a1d0b1962324701d77026afd
msn-official <m.senthilnathan@tvsmotor.com>
2020-11-12 15:51:36 +0530

    cors updated

--- notes.js
+++ notes.js
@@ -23,7 +23,7 @@ const app = express();
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*.tvsmotor.com");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
